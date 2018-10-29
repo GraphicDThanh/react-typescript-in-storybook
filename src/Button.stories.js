682cc3ee3903642f525f5e9538fdbca3dee5d60f
Thanh Nguyen <thanh.nguyen@asnet.com.vn>
2018-10-29 22:42:49 +0700

    use addon with info for storybook

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,12 +1,18 @@
 import React from 'react';
-
-import {storiesOf} from '@storybook/react'
+import { wInfo } from './utils'
+import { storiesOf } from '@storybook/react'
 import {Button} from './Button'
 
 storiesOf('Button', module)
-  .addWithJSX('with background pink', () => (
+  .addWithJSX('with background pink', wInfo(`
+    description
+
+    ~~~js
+    <Button>This is</Button>
+    ~~~
+    `)(() => (
     <Button bg="pink">Hello pink</Button>
-  ))
-  .add('with background yellow', () => (
+  )))
+  .addWithJSX('with background yellow', () => (
     <Button bg="yellow">Hello  Yellow</Button>
   ))
