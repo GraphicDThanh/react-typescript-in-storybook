f41653658dd780a2aa343b296eba902c501a3bfa
Thanh Nguyen <thanh.nguyen@asnet.com.vn>
2018-10-29 23:04:50 +0700

    use addon knobs of storybook

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { wInfo } from './utils'
 import { storiesOf } from '@storybook/react'
 import {Button} from './Button'
-
+import { text, color } from '@storybook/addon-knobs/react';
 storiesOf('Button', module)
   .addWithJSX('with background pink', wInfo(`
     description
@@ -11,8 +11,8 @@ storiesOf('Button', module)
     <Button>This is</Button>
     ~~~
     `)(() => (
-    <Button bg="pink">Hello pink</Button>
+    <Button bg={text('bg', 'pink')}>Hello pink</Button>
   )))
   .addWithJSX('with background yellow', () => (
-    <Button bg="yellow">Hello  Yellow</Button>
+    <Button bg={color('bg', 'yellow', 'group1')}>Hello  Yellow</Button>
   ))
